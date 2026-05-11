44ba0d059b6ec24f19db0f406664c7c6865ac46f
Bryan Dorregaray <abc.123dorregaray@gmail.com>
2026-05-11 16:17:27 -0500

    Modificación de mensaje de validacion

--- src/main/java/com/upc/mindcare/dtos/PacienteDTO.java
+++ src/main/java/com/upc/mindcare/dtos/PacienteDTO.java
@@ -19,19 +19,19 @@ public class PacienteDTO
 {
     private Long pacienteId;
 
-    @Min(value = 0, message = "Edad invÃ¡lida")
+    @Min(value = 0, message = "Edad invalida")
     private Integer edad;
 
-    @NotBlank(message = "El gÃ©nero es obligatorio")
+    @NotBlank(message = "El género es obligatorio")
     private String genero;
 
     @NotNull(message = "Fecha obligatoria")
     private LocalDate fechaNacimiento;
 
-    @Pattern(regexp = "^[0-9]{9}$", message = "TelÃ©fono debe tener 9 dÃ­gitos")
+    @Pattern(regexp = "^[0-9]{9}$", message = "El teléfono debe tener 9 dígitos")
     private String telefono;
 
-    @Pattern(regexp = "^[0-9]{9}$", message = "Contacto invÃ¡lido")
+    @Pattern(regexp = "^[0-9]{9}$", message = "Contacto inválido")
     private String contactoEmergencia;
 
     private Long usuarioId;
